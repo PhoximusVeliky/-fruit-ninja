d9fb1fac0b665c126797aa6b0c1ada3490b77756
PhoximusVeliky <144343315+PhoximusVeliky@users.noreply.github.com>
2024-05-12 10:53:49 +0300

    0.7
    добавлены параболы в виде массива

--- main.py
+++ main.py
@@ -32,6 +32,7 @@
 # root.mainloop()
 import tkinter as tk
 from collections import deque
+from array import *
 
 def redraw():
     if len(q_blade_xy) == 2 :
@@ -57,28 +58,7 @@ def redraw():
             canvas.create_oval(x-rad, y-rad, x+rad, y+rad, fill="red",tags="blade")
             if len(q_blade_xy)<((20+20/4)*2)-2:
                 q_blade_xy.append(x)
-                q_blade_xy.append(y)  
-
-def delete_circle():
-
-    if 2<= len(q_blade_xy):
-        while rad != 20 :
-            x,y = q_blade_xy.popleft(),q_blade_xy.popleft()
-            rad +=1
-            canvas.create_oval(x-rad, y-rad, x+rad, y+rad, fill="red",tags="blade")
-            if len(q_blade_xy)<((20+20/4)*2)-2:
-                q_blade_xy.append(x)
-                q_blade_xy.append(y)
-                
-    if rad==20:
-        while rad != 0 :
-            x,y = q_blade_xy.popleft(),q_blade_xy.popleft()
-            rad -=4
-            canvas.create_oval(x-rad, y-rad, x+rad, y+rad, fill="red",tags="blade")
-            if len(q_blade_xy)<((20+20/4)*2)-2:
-                q_blade_xy.append(x)
-                q_blade_xy.append(y)
-
+                q_blade_xy.append(y)                
 
 def draw_circle(event):
     global idle_timer
@@ -94,12 +74,30 @@ def clear_circles():
     canvas.delete("blade")
     q_blade_xy.clear()
 
+def coordinates_fruits():
+    global coordinates1_xy,coordinates4_xy,coordinates16_xy
+    x=-100
+    while x != 101: # иначе в 16 не хватает одного
+        if -25<=x<=25   and x*x % 1  == 0:
+            coordinates1_xy.extend([x,(x*x)//1])
+        if -50<=x<=50   and x*x % 4  == 0:
+            coordinates4_xy.extend([x,(x*x)//4])
+        if -100<=x<=100 and x*x % 16 == 0:
+            coordinates16_xy.extend([x,(x*x)//16])
+        x+=1
+
+
 
 root = tk.Tk()
 root.title("Draw Circle on Right Click")
 q_blade_xy = deque()
 idle_timer = root.after(1000, clear_circles) 
+coordinates1_xy,coordinates4_xy,coordinates16_xy = array('i'),array('i'),array('i')
 
+coordinates_fruits()
+print(len(coordinates1_xy))
+print(len(coordinates4_xy))
+print(len(coordinates16_xy))
 width, height = 800, 600
 root.geometry("900x700")
 canvas = tk.Canvas(root, width=width, height=height, bg="white")
